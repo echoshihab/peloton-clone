06657ca804836022fbd1c97c848d466ce67736fe
echoshihab <echoshihab@gmail.com>
2021-05-03 19:15:51 -0400

    configure tabbed view with FA icons

--- peloton-clone/peloton-clone/AssemblyInfo.cs
+++ peloton-clone/peloton-clone/AssemblyInfo.cs
@@ -1,4 +1,9 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+
+[assembly: ExportFont("FABrands.otf", Alias = "FABrands")]
+[assembly: ExportFont("FARegular.otf", Alias = "FARegular")]
+[assembly: ExportFont("FASolid.otf", Alias = "FASolid")]
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace System.Runtime.CompilerServices
