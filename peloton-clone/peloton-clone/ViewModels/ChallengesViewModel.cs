975949242976f5eaab146f3698ca69374ea372d5
echoshihab <echoshihab@gmail.com>
2021-05-11 15:50:49 -0400

    add tappable challange status toggler menu

--- peloton-clone/peloton-clone/ViewModels/ChallengesViewModel.cs
+++ peloton-clone/peloton-clone/ViewModels/ChallengesViewModel.cs
@@ -2,10 +2,35 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace PelotonCloneApp.ViewModels
 {
     public class ChallengesViewModel : BaseViewModel
-{
-}
+    {
+
+        public ChallengesViewModel()
+        {
+            SelectedcategoryCommand = new Command<string>(OnSelectCategory);
+
+        }
+
+        int selectedCategoryIndex;
+
+        public int SelectedCategoryIndex
+        {
+            get => selectedCategoryIndex;
+            set => SetProperty(ref selectedCategoryIndex, value);
+        }
+
+        public Command<string> SelectedcategoryCommand { get; }
+
+        void OnSelectCategory(string index)
+        {
+            if(int.TryParse(index, out int i))
+            {
+                SelectedCategoryIndex = i;
+            }
+        }
+    }
 }
